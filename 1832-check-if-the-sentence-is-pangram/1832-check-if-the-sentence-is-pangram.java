a49f68c34c4c56d34e6f479c7a10d4a100567b07
malakklabib <127633608+malakklabib@users.noreply.github.com>
2023-09-06 19:49:37 +0300

    Time: 3 ms (52.46%), Space: 40.7 MB (36.25%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -2,7 +2,9 @@ class Solution {
     public boolean checkIfPangram(String sentence) {
         Set<Character> alph = Set.of('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
                                     'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
-        Set<Character> charSet = sentence.chars().mapToObj(c ->(char)c).collect(Collectors.toSet());
+        Set<Character> charSet = new HashSet<Character>();
+        for(char c : sentence.toCharArray())
+            charSet.add(c);
         for(Character c : alph){
             if(!charSet.contains(c))
                 return false;

8354dc4f17e6eb541f3972af6de567844ec42356
malakklabib <127633608+malakklabib@users.noreply.github.com>
2023-11-15 22:49:45 +0200

    Time: 35 ms (66.67%), Space: 58.8 MB (19.62%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -3,21 +3,16 @@ class Solution {
         Map<Integer, Integer> counts = new HashMap<>();
         for(int i = 0; i < arr.length; i++)
             counts.put(arr[i], counts.getOrDefault(arr[i], 0) + 1);
-        Integer[] boxedArr = new Integer[arr.length];
-        for (int i = 0; i < arr.length; i++) 
-            boxedArr[i] = arr[i];
-        Arrays.sort(boxedArr, (a, b) -> counts.get(b)-counts.get(a));
         int res = 0;
         int length = 0;
-        for(int i: boxedArr){
-            if(!counts.isEmpty() && counts.containsKey(i)){
-                length+=counts.get(i);
-                counts.remove(i);
-                res++;
-            }
+        List<Integer> l = new ArrayList<Integer>(counts.values());
+        Collections.sort(l, Collections.reverseOrder());
+        for(int i: l){
+            length+=i;
+            res++;
             if(length >= arr.length/2)
                 break;
-        }   
+        }
         return res;
     }
 }

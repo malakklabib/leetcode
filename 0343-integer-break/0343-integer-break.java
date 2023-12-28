e567ef039d1f0bcc58af9fc540a1b6876eca86b6
malakklabib <127633608+malakklabib@users.noreply.github.com>
2023-12-28 14:16:03 +0200

    Time: 21 ms (5.16%), Space: 40.8 MB (6.26%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.java
@@ -0,0 +1,25 @@
+class Solution {
+    int[] memo;
+    public int integerBreak(int n) {
+        if(n==2) return 1;
+        memo = new int[n+1];
+        Arrays.fill(memo, -1);
+        
+        for(int i = n-1; i > 0; i--)
+            memo[n] = Math.max(memo[n], i * dp(n-i));
+        
+        for(int i:memo)
+            System.out.print(i + " ");
+
+        return memo[n];
+    }
+    public int dp(int i) {
+        if(i==0)
+            return 1;
+        if(memo[i] != -1)
+            return memo[i];
+        for(int j = i; j > 0; j--)
+            memo[i] = Math.max(memo[i], j * dp(i-j));
+        return memo[i];
+    }
+}

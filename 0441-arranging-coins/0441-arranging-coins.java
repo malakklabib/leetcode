7315dae682ff31dae0515b80cb7de57c00a2b957
malakklabib <127633608+malakklabib@users.noreply.github.com>
2024-01-26 00:51:42 +0200

    Time: 1 ms (98.87%), Space: 41.4 MB (8.06%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,10 +1,16 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int c = 0;
-        for(int i = 1; i <= n; i++){
-            n-=i;
-            c++;
+        int low = 1, high = n, mid = n;
+        while(low <= high){
+            mid = low + (high-low)/2;
+            long coins = (long)mid*(mid+1)/2;
+            if(coins == n)
+                return mid;
+            else if(coins < n)
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
-        return c;
+        return high;
     }
 }

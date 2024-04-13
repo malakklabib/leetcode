65b174500f4afb4080ae21922d4cc3b09eff1939
malakklabib <127633608+malakklabib@users.noreply.github.com>
2024-04-13 22:59:03 +0200

    Time: 6 ms (69.50%), Space: 43.8 MB (74.48%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.java
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -1,16 +1,39 @@
 class Solution {
+    Integer[][] memo;
+    int[] nums1;
+    int[] nums2;
     public int maxUncrossedLines(int[] n1, int[] n2) {
-        int[][] dp = new int[n1.length+1][n2.length+1];
-        dp[n1.length][n2.length] = 0;
-        int connect = 0, skip1 = 0, skip2 = 0;
-        for(int i = n1.length-1; i >= 0; i--) {
-            for(int j = n2.length-1; j >= 0; j--) {
-                if(n1[i] == n2[j])
-                    dp[i][j] = 1 + dp[i+1][j+1];
-                else
-                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j+1]);
+        nums1 = n1;
+        nums2 = n2;
+        memo = new Integer[n1.length][n2.length];
+        return dp(0,0);
+    }
+    
+    public int dp(int i, int j){
+        if(i==nums1.length || j==nums2.length)
+            return 0;
+        
+        if(memo[i][j]!=null)
+            return memo[i][j];
+    
+        int take = 0;
+        int index = -1;
+        for(int k = j; k < nums2.length; k++){
+            if(nums1[i]==nums2[k]){
+                take = 1;
+                index = k;
+                break;
             }
         }
-        return dp[0][0];
+        if(index!=-1)
+            take += dp(i+1,index+1);
+        else
+            take += dp(i+1,j);
+        int skip = dp(i+1,j);
+        
+        memo[i][j] = Math.max(skip, take);
+        
+        return memo[i][j];
     }
+
 }

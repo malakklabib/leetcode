8e32a775fd0dc808c86ead390444fb8dedbbf837
malakklabib <127633608+malakklabib@users.noreply.github.com>
2024-07-01 17:14:20 +0300

    Time: 56 ms (81.15%), Space: 62.4 MB (5.42%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        Map<Integer, Integer> map = new HashMap();
+
+        int n = position.length;
+        
+        for(int i = 0; i < n; i++){
+            map.put(position[i], speed[i]);
+        }
+        
+        Arrays.sort(position);
+        
+        int ans = 1;
+        double slowest = (double)(target-position[n-1])/map.get(position[n-1]);
+        System.out.print(slowest+" ");
+        
+        for(int i = n-2; i >= 0; i--){
+            double currTime = (double)(target-position[i])/map.get(position[i]);
+            if(currTime > slowest){
+                ans++;
+                slowest = currTime;
+            }
+        }
+        
+        return ans;
+    }
+}

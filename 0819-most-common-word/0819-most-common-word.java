8e9ad32ec2269af3933e0a2c5ac91235f7a0b893
malakklabib <127633608+malakklabib@users.noreply.github.com>
2024-09-18 19:17:58 +0300

    Time: 30 ms (5.29%), Space: 43.9 MB (10.36%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        Map<String, Integer> counts = new HashMap<>();
+        Set<String> bannedSet = new HashSet<>();
+        for(String word: banned)
+            bannedSet.add(word);
+        
+        int maxCount = 1;
+        for(String word: paragraph.split("[\\s!?',;.]+")){
+            String lowercaseWord = word.toLowerCase();
+            if(bannedSet.contains(lowercaseWord))
+                continue;
+            counts.put(lowercaseWord, counts.getOrDefault(lowercaseWord, 0) + 1);
+            maxCount = Math.max(maxCount, counts.get(lowercaseWord));
+            System.out.println(lowercaseWord+" "+ maxCount);
+        }
+        
+        for(Map.Entry<String, Integer> entry: counts.entrySet()){
+            if(entry.getValue()==maxCount)
+                return entry.getKey();
+        }
+        
+        return "";
+    }
+}
